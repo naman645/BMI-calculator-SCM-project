d5679236092abb59cb3067b0d6a7dd7a5102b6b2
Navjinder Singh <158038419+Navjinder28Multani@users.noreply.github.com>
2024-05-05 12:11:57 +0530

    Update app.js

--- app.js
+++ app.js
@@ -28,6 +28,7 @@ function calculateBmi() {
 //bmi < 18.5 the person is consider to be healthy.
     } else if (bmi >= 25 && bmi < 29.9) {
 //bmi >= 25 the person is consider to be overweight.
+//bmi < 29.9 the person is consider to be overweight.
         result.innerText = `Over Weight: ${bmi}`;
     } else if (bmi >= 30 && bmi < 34.9) {
         result.innerText = `Obesity (Class I): ${bmi}`;
