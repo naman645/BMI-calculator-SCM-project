ba56561f41d1e0b7bd3f690a203d1c2144d6e1af
Navjinder Singh <158038419+Navjinder28Multani@users.noreply.github.com>
2024-05-15 13:17:56 +0530

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ window.onload = () => {
     const button = document.querySelector('#btn');
     button.addEventListener('click', calculateBmi)
 }
-// this is the function used for calculation
+
 
 function calculateBmi() {
     const height = document.querySelector('#height').value;
